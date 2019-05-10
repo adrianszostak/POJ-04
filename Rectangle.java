cb62c58cff3894f9927b70d7b59409ae23afbfc0
adrianszostak <48855378+adrianszostak@users.noreply.github.com>
2019-05-10 21:42:27 +0200

    Zad 4

---
+++ Rectangle.java
@@ -0,0 +1,52 @@
+public class Rectangle extends Shape {
+
+    protected double width = 1.0;
+    protected double length = 1.0;
+    protected double area;
+    protected double perimeter;
+
+    public Rectangle (String color, boolean filled, double width, double length)
+    {
+        super (color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth ()
+    {
+        return width;
+    }
+
+    public void setWidth (double width)
+    {
+        this.width = width;
+        width = 1.0;
+    }
+
+    public double getLength ()
+    {
+        return length;
+    }
+
+    public void setLength (double length)
+    {
+        this.length = length;
+        length = 1.0;
+    }
+
+    public double getArea ()
+    {
+        return area;
+    }
+
+    public double getPerimeter ()
+    {
+        return perimeter;
+    }
+
+    public void tooooString ()
+    {
+        System.out.println("Color is " + color + " Filled: " + filled + " Width = " + width + " Length = " + length
+                + " Area = " + area + " Perimeter = " + perimeter);
+    }
+}
